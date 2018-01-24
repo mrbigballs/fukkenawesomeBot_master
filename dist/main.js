7ae5b16f0f78487e59fe94ebc0f33c9b88a3befe
André Gluderer <andre@Andres-MacBook-Pro.local>
2018-01-24 19:44:04 +0100

    compile on mac :D

--- dist/main.js
+++ dist/main.js
@@ -112,6 +112,10 @@ electron_1.ipcMain.on('botConnected', function (event, arg) {
     console.log(arg);
     systemTray.setImage(trayIconOnline);
 });
+electron_1.ipcMain.on('botDisconnected', function (event, arg) {
+    console.log(arg);
+    systemTray.setImage(trayIconOffline);
+});
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here. 
 //# sourceMappingURL=main.js.map
